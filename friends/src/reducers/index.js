a52024998ede3fdbeb1807ae521bf183afc38140
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-04-11 16:08:43 -0500

    init rootReducer and store wired up in index.js

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,13 @@
+
+
+const initialState = {
+    friends : []
+}
+
+
+export const rootReducer = (state=initialState, action) => {
+    switch(action) {
+        default:
+            return state;
+    }
+}
